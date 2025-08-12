5d66eba1aabd410d066b3b055972859726d8d4f3
Shadows Conception <tassiesboys@gmail.com>
2025-08-12 17:13:36 -0600

    chore(types): add mammoth.extractRawText to shim

--- src/types/mammoth.d.ts
+++ src/types/mammoth.d.ts
@@ -1,10 +1,16 @@
 declare module "mammoth" {
+  type Input =
+    | { arrayBuffer: ArrayBuffer }
+    | { path: string }
+    | { buffer: Buffer | Uint8Array | ArrayBuffer };
+
+  type ConvertResult = { value: string; messages?: any[] };
+
   const mammoth: {
-    convertToHtml(
-      input: { arrayBuffer: ArrayBuffer } | { path: string },
-      options?: Record<string, any>
-    ): Promise<{ value: string; messages?: any[] }>;
+    convertToHtml(input: Input, options?: Record<string, any>): Promise<ConvertResult>;
+    extractRawText(input: Input, options?: Record<string, any>): Promise<ConvertResult>;
   };
+
   export = mammoth;
   export default mammoth;
 }
